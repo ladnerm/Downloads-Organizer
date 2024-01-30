65dd6bb2aeb6b2648abacb87f1a950893b36cd5a
Max Ladner <ladnerm78@gmail.com>
2024-01-29 20:47:16 -0500

    changes

--- Main.py
+++ Main.py
@@ -3,35 +3,40 @@ import shutil
 import sys
 
 
-dwnldPath = "/Users/maxladner/Downloads"
-imagePath = "/Users/maxladner/Documents/Downloaded Images"
-videoPath = "/Users/maxladner/Documents/Downloaded Videos"
-docsPath = "/Users/maxladner/Documents/Downloaded Docs"
-musicPath = "/Users/maxladner/Documents/Downloaded Music"
-miscellaneousPath = "/Users/maxladner/Documents/Miscellaneous"
+dwnldPath = "/Users/USERNAME/Downloads"
+imagePath = "/Users/USERNAME/Documents/Images"
+videoPath = "/Users/USERNAME/Documents/Videos"
+docsPath = "/Users/USERNAME/Documents/Docs"
+musicPath = "/Users/USERNAME/Documents/Music"
+
+
+def checkDir(path):
+	if not os.path.isdir(path):
+		os.mkdir(path)
 
 def move(oldPath, newPath):
 	shutil.move(oldPath, newPath)
 
 
 def LoggingEventHandler():
+
+
 	with os.scandir(dwnldPath) as files:
 		for file in files:
 			if file.name.endswith((".jpg", ".jpeg", ".HEIC", ".heic")):
+				checkDir(imagePath)
 				move(file, imagePath)
 			elif file.name.endswith((".mov", ".mp4", ".MP4")):
+				checkDir(videoPath)
 				move(file, videoPath)
 			elif file.name.endswith((".mp3", ".m4a")):
+				checkDir(musicPath)
 				move(file, musicPath)
 			elif file.name.endswith((".png", ".pdf")):
+				checkDir(docsPath)
 				move(file, docsPath)
-			elif file.name == ".DS_Store":
-				pass
 			else:
-				move(file, miscellaneousPath)
+				pass
 
-
-
-			
 
 LoggingEventHandler()
